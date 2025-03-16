b2c4fd65c173e36255d8e038a1e87ea597071332
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-03-16 12:20:40 -0300

    Add files via upload
    PF_first_Assignement_program_1

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,small,grater;
+	cout<<"enetr the first number"<<a<<endl;
+	cin>>a;
+	cout<<"enter the secound number"<<b<<endl;
+	cin>>b;
+	small=a<b;
+	grater=a>b;
+	cout<<"the result is given\t"<<small<<grater<<endl;
+	return 0;
+	
+}
