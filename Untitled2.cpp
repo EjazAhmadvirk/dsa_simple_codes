7f400d22349dbee6d772348e798941956ac67336
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-03-16 12:21:18 -0300

    Add files via upload
    Assignemet_file_2

---
+++ Untitled2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int temp=10;
+	char ch=20;
+
+	double doub=5.78;
+	cout<<"temp\t"<<temp<<endl;
+	cout<<"ch\t"<<ch<<endl;
+	cout<<"doub\t"<<doub<<endl;
+	return 0;
+}
