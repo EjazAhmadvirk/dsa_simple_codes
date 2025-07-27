bb12ae7471fe0ad83c5d2fc65ba73bf22d3f9026
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-07-27 09:10:05 +0500

    Create orderMain.java

---
+++ orderMain.java
@@ -0,0 +1,10 @@
+package labtasks;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        OnlineOrder o = new OnlineOrder();
+        System.out.println(o.Customerinformation());
+    }
+}
