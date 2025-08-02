79c1940454d368aca416bbaa9fb7e7a009eddc9b
Ejaz Ahmad <ejazahmadvirk091@gmail.com>
2025-08-02 23:04:59 +0500

    Queus.ts

---
+++ Queu-in-python/Queus.ts
@@ -0,0 +1,110 @@
+// Node class
+class Node {
+    data: number;
+    next: Node | null;
+
+    constructor(val: number) {
+        this.data = val;
+        this.next = null;
+    }
+}
+
+// Circular Linked List class
+class CircularList {
+    private tail: Node | null;
+
+    constructor() {
+        this.tail = null;
+    }
+
+    // Push at front
+    pushFront(val: number): void {
+        const newNode = new Node(val);
+        if (!this.tail) {
+            this.tail = newNode;
+            this.tail.next = this.tail;
+        } else {
+            newNode.next = this.tail.next;
+            this.tail.next = newNode;
+        }
+    }
+
+    // Push at back
+    pushBack(val: number): void {
+        const newNode = new Node(val);
+        if (!this.tail) {
+            this.tail = newNode;
+            this.tail.next = this.tail;
+        } else {
+            newNode.next = this.tail.next;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    // Pop from front
+    popFront(): void {
+        if (!this.tail) {
+            console.log("List is empty!");
+            return;
+        }
+        const head = this.tail.next;
+        if (head === this.tail) {
+            this.tail = null;
+        } else if (head) {
+            this.tail.next = head.next;
+        }
+    }
+
+    // Pop from back
+    popBack(): void {
+        if (!this.tail) {
+            console.log("List is empty!");
+            return;
+        }
+        const head = this.tail.next;
+        if (head === this.tail) {
+            this.tail = null;
+        } else if (head) {
+            let temp = head;
+            while (temp.next !== this.tail) {
+                temp = temp.next!;
+            }
+            temp.next = this.tail.next;
+            this.tail = temp;
+        }
+    }
+
+    // Display list
+    display(): void {
+        if (!this.tail) {
+            console.log("List is empty!");
+            return;
+        }
+        let temp = this.tail.next;
+        const head = temp;
+        let result = '';
+        do {
+            result += temp!.data + " -> ";
+            temp = temp!.next;
+        } while (temp !== head);
+        console.log(result + "(head)");
+    }
+}
+
+// Test the Circular Linked List
+const cll = new CircularList();
+
+cll.pushBack(10);
+cll.pushFront(5);
+cll.pushBack(20);
+cll.display();  // 5 -> 10 -> 20 -> (head)
+
+cll.popFront();
+cll.display();  // 10 -> 20 -> (head)
+
+cll.popBack();
+cll.display();  // 10 -> (head)
+
+cll.popBack();
+cll.display();  // List is empty!
